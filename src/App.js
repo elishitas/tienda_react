ed8bd29309813882c25fde7bd5fab947a5750ce1
Maria Elisa Araya <arayamariaelisa@gmail.com>
2021-02-08 15:39:33 -0300

    instalacion y configuracion del entorno

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React y probando nuevamente
         </a>
       </header>
     </div>

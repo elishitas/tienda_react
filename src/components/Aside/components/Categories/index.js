9801e2e4fcf3c2112e5964c52005b45a754c8b00
Maria Elisa Araya <arayamariaelisa@gmail.com>
2021-02-08 22:27:26 -0300

    se agrega validacion de categorias

--- src/components/Aside/components/Categories/index.js
+++ src/components/Aside/components/Categories/index.js
@@ -2,11 +2,16 @@
 import React from 'react';
 
 const Categories = (props) => {
+
+    const existCategories = props.categories.lenght > 0;
+
     return (
         <div className="aside-categories">
             <div className="categories-title">Categorías</div>
             <ul className="categories-list">
               {props.categories.map(category => (<li>{category.name}</li>))}
+              :
+              <p>No existen categrias</p>
             </ul>
         </div>
     )

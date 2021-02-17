32c4d3aac9ee16e42dbc84e399df1ac563bfc0d3
Maria Elisa Araya <arayamariaelisa@gmail.com>
2021-02-17 09:50:56 -0300

    se rompe con el SideBar

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ProductsWrapper from '../../components/ProductsWrapper';
-import SideBar from '../../components/SideBar';
+//import SideBar from '../../components/SideBar';
 
 const Home = ({ match, history }) => {
   return (
     <React.Fragment>
-      <SideBar params={match.params} />
+
       <ProductsWrapper history={history} params={match.params} />
     </React.Fragment>    
   );
